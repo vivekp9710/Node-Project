a214dc1773ef816a06a7c45dbc0b5202ee0fffa5
Vivek patel <115336771+vivekp9710@users.noreply.github.com>
2024-07-25 11:06:16 +0530

    Delete Node Project directory

--- Node Project/src/model/index.js
+++
@@ -1,8 +0,0 @@
-import { User } from "./user";
-// console.log("------User-------", User);
-import { Post } from "./post";
-import { Like } from "./like";
-import { SavePost } from "./savedPost";
-import { Follower } from "./follower";
-
-export const modals = { User, Post, Like, SavePost, Follower };
